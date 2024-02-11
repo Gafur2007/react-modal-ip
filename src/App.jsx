0e290e233de618c3dfc161c5db86f29d05002d2e
Gafur2007 <tohirovabdugofur@gmail.com>
2024-02-11 20:55:46 +0500

    react-modal-ip1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Clients from "./Components/Clients/Clients";
+import What from "./Components/What/What";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Clients />
+      <What />
     </>
   );
 }
